bc2d48f262b61af9838f7a5e4c59e4655825bdca
Siddhant Kumar <Sid@Prateeks-MacBook-Pro.local>
2020-09-01 22:23:17 -0700

    v.2.1

--- js/script.js
+++ js/script.js
@@ -1,75 +1,29 @@
-// var bar = new ProgressBar.Line(progress, {
-//     strokeWidth: 4,
-//     easing: 'easeInOut',
-//     duration: 1400,
-//     color: '#ff5c33',
-//     trailColor: '#eee',
-//     trailWidth: 1,
-//     svgStyle: {width: '100%', height: '100%'}
-//   });
+var options = {
+  strings: ['Software Engineer', 'Web Developer', 'Software Engineer | Web Developer'],
+  typeSpeed: 40,
+  fadeOut: false,
+  showCursor: false
+};
   
-//   bar.animate(0.9);  // Number from 0.0 to 1.0
+var typed = new Typed('.element', options);
 
-//   var bar = new ProgressBar.Line(progresstwo, {
-//     strokeWidth: 4,
-//     easing: 'easeInOut',
-//     duration: 1400,
-//     color: '#ff5c33',
-//     trailColor: '#eee',
-//     trailWidth: 1,
-//     svgStyle: {width: '100%', height: '100%'}
-//   });
-  
-//   bar.animate(0.9);  // Number from 0.0 to 1.0
 
-//   var bar = new ProgressBar.Line(progressthree, {
-//     strokeWidth: 4,
-//     easing: 'easeInOut',
-//     duration: 1400,
-//     color: '#ff5c33',
-//     trailColor: '#eee',
-//     trailWidth: 1,
-//     svgStyle: {width: '100%', height: '100%'}
-//   });
-  
-//   bar.animate(0.8);  // Number from 0.0 to 1.0
+topButton = document.getElementById("topbtn")
 
-//   var bar = new ProgressBar.Line(progressfour, {
-//     strokeWidth: 4,
-//     easing: 'easeInOut',
-//     duration: 1400,
-//     color: '#ff5c33',
-//     trailColor: '#eee',
-//     trailWidth: 1,
-//     svgStyle: {width: '100%', height: '100%'}
-//   });
-  
-//   bar.animate(0.9);  // Number from 0.0 to 1.0
+window.onscroll = function() {
+  scrollFunction()
+};
 
-//   var bar = new ProgressBar.Line(progressfive, {
-//     strokeWidth: 4,
-//     easing: 'easeInOut',
-//     duration: 1400,
-//     color: '#ff5c33',
-//     trailColor: '#eee',
-//     trailWidth: 1,
-//     svgStyle: {width: '100%', height: '100%'}
-//   });
-  
-//   bar.animate(0.8);  // Number from 0.0 to 1.0
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  }
+  else {
+    topButton.style.display = "none";
+  }
+}
 
-// //   var typed = new Typed('#typed', {
-// //     stringsElement: '#typed-strings'
-// //   });
-
-  var options = {
-    strings: ['Software Engineer', 'Web Developer', 'Software Engineer | Web Developer'],
-    typeSpeed: 40,
-    fadeOut: false,
-    showCursor: false
-    // backSpeed: 0,
-    // onStop: (arrayPos, self) => {document.getElementByClassName('element').innerHTML = ""}
-  };
-  
-  var typed = new Typed('.element', options);
-
+topButton.onclick = function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
